7921e660087cfc0fe44a43c0d3bbaf8967e6b0fd
Klaus Kupper <klausdk1999@gmail.com>
2022-09-24 16:30:29 -0300

    init cypress

---
+++ front-end/cypress/factory/factory.ts
@@ -0,0 +1,8 @@
+export function recommendationData(score: number): object {
+    return {
+        id: 434,
+        name: "Teste",
+        youtubeLink: "https://www.youtube.com/watch?v=hjz6-opHCBc",
+        score,
+    };
+}
